616eb54d28084453fa68149271e9e888f78b98b8
nico <nicolas.noya.anido@udc.es>
2025-10-16 21:28:50 +0200

    funciones shell hechas

--- main.c
+++ main.c
@@ -48,9 +48,91 @@ void rellenar_v_desc(int v[], int n) {
     }
 }
 
-void rellenar_v_hibbard(int v[], int m, int max) {
+void rellenar_v_hibbard(int inc[], int *m, int n) {
 
+    int k = 1;
+    int val = 1;
+    int i = 0;
+    int temp[50];
+
+    // Generar secuencia ascendente
+    while (val < n) {
+        temp[i++] = val;
+        k++;
+        val = (1 << k) - 1;  // más rápido y limpio que pow
+    }
+
+    // Invertirla en inc[]
+    for (int j = 0; j < i; j++) {
+        inc[j] = temp[i - j - 1];
+    }
+
+    *m = i; // tamaño real
+
+    // Mostrar (opcional)
+    for (int j = 0; j < *m; j++) {
+        printf("%d ", inc[j]);
+    }
+
+    printf("\n");
 }
+
+void rellenar_v_knuth(int v[], int m, int max) {
+
+    int k, i;
+    for (k = 1, i = 0; k <= max && i < m; k = 3 * k + 1, i++) {
+        v[i] = k;
+    }
+}
+
+void rellenar_v_sedgewick(int v[], int m, int max) {
+    int h = 1;
+    int i = 0;
+
+    for (h = 1, i = 0; h <= max && i < m; i++) {
+        v[i] = h;
+        h = pow(4, i) + 3 * pow(2, i - 1) + 1;
+        if (h >= 500) break; 
+    }
+}
+
+void rellenar_v_ciura(int inc[], int *m, int n) {
+    int base[] = {1, 4, 10, 23, 57, 132, 301, 701, 1750};
+    int i, k = 9; // hay 9 valores base
+    int next;
+
+    // Copiar los valores base
+    for (i = 0; i < k && base[i] < n; i++) {
+        inc[i] = base[i];
+    }
+
+    next = base[k - 1];
+
+    // Ampliar con multiplicaciones por 2.25
+    while ((int)(next * 2.25) < n && i < 50) {  // por si acaso no te pasas de tamaño
+        next = (int)(next * 2.25 + 0.5); // redondeo
+        inc[i++] = next;
+    }
+
+    // Invertir orden, porque Shell sort empieza con los gaps grandes
+    for (int j = 0; j < i / 2; j++) {
+        int tmp = inc[j];
+        inc[j] = inc[i - j - 1];
+        inc[i - j - 1] = tmp;
+    }
+
+    *m = i;
+
+    // Mostrar para comprobar
+    for (int j = 0; j < *m; j++) {
+        printf("%d ", inc[j]);
+    }
+    printf("\n");
+}
+
+
+
+
 
 void ord_ins(int v[], int n) {
     int i, j, x;
@@ -161,11 +243,15 @@ void insercion_test() {
 }
 
 void shell_test() {
-    int n, m = 10;
+    int n = 32000; 
+    int m = 50;
     int v[n];
     int inc[m];
 
-    rellenar_v_hibbard(inc, m, n);
+    rellenar_v_hibbard(inc, &m, n);
+    rellenar_v_knuth(inc, m, n);
+    rellenar_v_sedgewick(inc, m, n);
+    rellenar_v_ciura(inc, &m, n);
 
 
 }
@@ -173,7 +259,7 @@ void shell_test() {
 
 int main() {
     inicializar_semilla();
-    insercion_test();
+    // insercion_test();
     shell_test();
 
     return 0;
